e7935ce29e4aa2d62e36f10ec0f5e707f8c0d35a
IgorSlatt <93879319+igorSzachista1@users.noreply.github.com>
2024-11-14 07:43:21 +0100

    Delete exercise 1.1 directory

--- exercise 1.1/src/Total.jsx
+++
@@ -1,8 +0,0 @@
-const Total = (props) => {
-    const propsArray = props.partsArray;
-    const totalAmount = propsArray.reduce((item, addNum) => item + addNum);
-    return <h3> Total amount of exercises:{totalAmount}</h3>;
-  };
-  
-  export default Total;
-  
